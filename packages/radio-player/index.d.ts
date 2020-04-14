8bd4c2312d3373ffbf69e1b104402de4c97e8c07
Jason Buckner <jason@jasonbuckner.com>
2020-04-14 14:20:19 -0700

    Export some additional classes and types

--- packages/radio-player/index.d.ts
+++ packages/radio-player/index.d.ts
@@ -1,15 +1,15 @@
-import RadioPlayer from './lib/src/radio-player.d';
-import SearchResultsSwitcher from './lib/src/search-results-switcher.d';
-import MusicZone from './lib/src/models/music-zone.d';
-import RadioPlayerConfig from './lib/src/models/radio-player-config.d';
-import { FullTextSearchServiceInterface } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-service-interface.d';
-import { SearchBackendInterface } from './lib/src/search-handler/search-backends/search-backend-interface.d';
-
 export {
-  RadioPlayer,
-  SearchResultsSwitcher,
-  MusicZone,
-  RadioPlayerConfig,
-  FullTextSearchServiceInterface,
-  SearchBackendInterface
-};
+  TranscriptConfig,
+  TranscriptEntryConfig
+} from '@internetarchive/transcript-view';
+export { AudioSource } from '@internetarchive/audio-element';
+
+export { RadioPlayer } from './lib/src/radio-player.d';
+export { RadioPlayerConfig } from './lib/src/models/radio-player-config.d';
+
+export { SearchHandler } from './lib/src/search-handler/search-handler.d';
+export { LocalSearchBackend } from './lib/src/search-handler/search-backends/local-search-backend/local-search-backend.d';
+export { FullTextSearchBackend } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-backend.d';
+export { TranscriptIndex } from './lib/src/search-handler/transcript-index.d';
+export { FullTextSearchResponse } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-response.d';
+export { FullTextSearchResponseDoc } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-response.d';
