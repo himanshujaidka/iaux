74af68fe1ce7dd65f9e003d27174e9cdcc2a7d49
Jason Buckner <jason@jasonbuckner.com>
2020-04-14 15:11:54 -0700

    More reorg
    To bump the version

--- packages/radio-player/index.d.ts
+++ packages/radio-player/index.d.ts
@@ -8,10 +8,11 @@ export { RadioPlayer } from './lib/src/radio-player.d';
 export { RadioPlayerConfig } from './lib/src/models/radio-player-config.d';
 
 export { SearchBackendInterface } from './lib/src/search-handler/search-backends/search-backend-interface.d';
+export { LocalSearchBackend } from './lib/src/search-handler/search-backends/local-search-backend/local-search-backend.d';
 
 export { SearchHandler } from './lib/src/search-handler/search-handler.d';
 export { SearchHandlerInterface } from './lib/src/search-handler/search-handler-interface.d';
-export { LocalSearchBackend } from './lib/src/search-handler/search-backends/local-search-backend/local-search-backend.d';
+
 export { TranscriptIndex } from './lib/src/search-handler/transcript-index.d';
 
 export { FullTextSearchBackend } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-backend.d';
