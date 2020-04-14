04891e5e343a864dc1443b6fcb4cfa5ee427a5ab
Jason Buckner <jason@jasonbuckner.com>
2020-04-14 15:16:36 -0700

    Update a few more declaration exports

--- packages/radio-player/index.d.ts
+++ packages/radio-player/index.d.ts
@@ -10,12 +10,12 @@ export { RadioPlayerConfig } from './lib/src/models/radio-player-config.d';
 export { SearchBackendInterface } from './lib/src/search-handler/search-backends/search-backend-interface.d';
 export { LocalSearchBackend } from './lib/src/search-handler/search-backends/local-search-backend/local-search-backend.d';
 
-export { SearchHandler } from './lib/src/search-handler/search-handler.d';
-export { SearchHandlerInterface } from './lib/src/search-handler/search-handler-interface.d';
-
-export { TranscriptIndex } from './lib/src/search-handler/transcript-index.d';
-
 export { FullTextSearchBackend } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-backend.d';
 export { FullTextSearchResponse } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-response.d';
 export { FullTextSearchResponseDoc } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-response.d';
 export { FullTextSearchServiceInterface } from './lib/src/search-handler/search-backends/full-text-search-backend/full-text-search-service-interface.d';
+
+export { SearchHandler } from './lib/src/search-handler/search-handler.d';
+export { SearchHandlerInterface } from './lib/src/search-handler/search-handler-interface.d';
+
+export { TranscriptIndex } from './lib/src/search-handler/transcript-index.d';
